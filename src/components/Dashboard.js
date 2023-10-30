e279e84d7b649f5178ce39a8cb3ff882e8c241a2
eusoncode <git@github.com:eusoncode/Kata.git>
2023-10-29 18:44:58 -0600

    Binding This

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -33,9 +33,9 @@ class Dashboard extends Component {
   };
 
   selectPanel(id) {
-    this.setState({
-     focused: id
-    });
+    this.setState(previousState => ({
+      focused: previousState.focused !== null ? null : id
+    }));
   }
 
   render() {
@@ -54,7 +54,7 @@ class Dashboard extends Component {
           id={panel.id}
           label={panel.label}
           value={panel.value}
-          onSelect={this.selectPanel}
+          onSelect={event => this.selectPanel(panel.id)}
         />
     ));
 
